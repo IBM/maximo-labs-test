5265999b74f4bf0d68dd2375d975f8ae4d1aebbd
Jan Ekstrøm <jan.ekstrom@dk.ibm.com>
2024-01-04 19:07:18 +0100

    added simple bash script to test in action
    Signed-off-by: Jan Ekstrøm <jan.ekstrom@dk.ibm.com>

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+##################################################################################
+#### DON't CHANGE THE BELOW MANDATORY SECTION WHICH IS A PRE-REQ FOR THE LABS ####
+##################################################################################
+
+# Set the Root directory, which is where this file is located.
+root_dir=`pwd`
+
+echo "Current directory: $root_dir"
+
+# Clear the existing /site content:
+echo "BUILD_INFO - The existing /site has been removed to avoid any leftovers."
+
+ls -la
